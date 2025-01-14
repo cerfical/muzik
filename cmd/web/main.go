fdb8d625ea4bb634a143363b586d1382c3697f48
cerfical <138404639+cerfical@users.noreply.github.com>
2025-01-14 23:45:34 +0500

    add command line arguments to provide basic customization

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,25 +2,31 @@ package main
 
 import (
 	"context"
+	"os"
 
+	"github.com/cerfical/muzik/internal/args"
 	"github.com/cerfical/muzik/internal/httpserv"
 	"github.com/cerfical/muzik/internal/log"
 	"github.com/cerfical/muzik/internal/memstore"
 )
 
 func main() {
-	servAddr := "127.0.0.1:8080"
 	log := log.New()
-
 	ctx := context.Background()
+	args := args.Parse(os.Args)
+
+	log = log.WithLevel(args.LogLevel)
 	server := httpserv.Server{
-		Addr:       servAddr,
+		Addr:       args.ServerAddr,
 		TrackStore: &memstore.TrackStore{},
 		Log:        log,
 	}
-	log.WithString("addr", servAddr).Info(ctx, "server startup")
+
+	log.WithString("addr", args.ServerAddr).
+		Info(ctx, "server startup")
 
 	if err := server.Run(); err != nil {
-		log.WithError(err).Fatal(ctx, "server shutdown")
+		log.WithError(err).
+			Fatal(ctx, "server shutdown")
 	}
 }
