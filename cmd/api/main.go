a1dd866e06428044fa4b89fee53ef75afc474ad1
cerfical <138404639+cerfical@users.noreply.github.com>
2025-01-19 19:17:39 +0500

    feat: expose configuration settings via a config file

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,20 +3,36 @@ package main
 import (
 	"os"
 
-	"github.com/cerfical/muzik/internal/args"
+	"github.com/cerfical/muzik/internal/config"
 	"github.com/cerfical/muzik/internal/httpserv"
 	"github.com/cerfical/muzik/internal/httpserv/middleware"
 	"github.com/cerfical/muzik/internal/log"
 	"github.com/cerfical/muzik/internal/memstore"
 )
 
+type Config struct {
+	Log struct {
+		Level log.Level `mapstructure:"level"`
+	} `mapstructure:"log"`
+}
+
 func main() {
 	log := log.New().WithContextKey(middleware.RequestID)
-	args := args.Parse(os.Args)
 
-	log = log.WithLevel(args.LogLevel)
+	config, err := config.Load(os.Args)
+	if err != nil {
+		log.WithError(err).
+			Fatal("error loading config file")
+	}
+
+	log = log.WithLevel(config.Log.Level)
+	log.WithFields(
+		"server.addr", config.Server.Addr,
+		"log.level", config.Log.Level,
+	).Info("Using config")
+
 	server := httpserv.Server{
-		Addr:       args.ServerAddr,
+		Addr:       config.Server.Addr,
 		TrackStore: &memstore.TrackStore{},
 		Log:        log,
 	}
@@ -24,19 +40,15 @@ func main() {
 	server.Use(middleware.LogRequest(log))
 	server.Use(middleware.AddRequestID)
 
-	log.WithFields("addr", server.Addr).
-		Info("Starting up the server")
-
-	defer func() {
-		log.Info("Shutting down the server")
-		if err := server.Close(); err != nil {
-			log.WithError(err).
-				Error("Server shutdown failed")
-		}
-	}()
-
+	log.Info("Starting up the server")
 	if err := server.Run(); err != nil {
 		log.WithError(err).
-			Error("Server terminated abnormally")
+			Fatal("Server terminated abnormally")
+	}
+
+	log.Info("Shutting down the server")
+	if err := server.Close(); err != nil {
+		log.WithError(err).
+			Fatal("Server shutdown failed")
 	}
 }
---
+++ internal/config/config.go
@@ -0,0 +1,45 @@
+package config
+
+import (
+	"errors"
+
+	"github.com/cerfical/muzik/internal/log"
+	"github.com/mitchellh/mapstructure"
+	"github.com/spf13/viper"
+)
+
+func Load(args []string) (*Config, error) {
+	if len(args) != 2 {
+		return nil, errors.New("expected a config path as the only command line argument")
+	}
+	return readFrom(args[1])
+}
+
+type Config struct {
+	Server struct {
+		Addr string `mapstructure:"addr"`
+	} `mapstructure:"server"`
+
+	Log struct {
+		Level log.Level `mapstructure:"level"`
+	} `mapstructure:"log"`
+}
+
+func readFrom(configPath string) (*Config, error) {
+	v := viper.New()
+	v.SetConfigFile(configPath)
+
+	if err := v.ReadInConfig(); err != nil {
+		return nil, err
+	}
+
+	// Apply defaults
+	var cfg Config
+	cfg.Log.Level = log.LevelInfo
+	cfg.Server.Addr = "localhost:80"
+
+	if err := v.Unmarshal(&cfg, viper.DecodeHook(mapstructure.TextUnmarshallerHookFunc())); err != nil {
+		return nil, err
+	}
+	return &cfg, nil
+}
