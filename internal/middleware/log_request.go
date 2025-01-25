1e843df4d9d78e1277706a602995f150d4e985a8
cerfical <138404639+cerfical@users.noreply.github.com>
2025-01-25 18:44:03 +0500

    feat(log): add request-id to differentiate entries among requests

--- internal/middleware/log_request.go
+++ internal/middleware/log_request.go
@@ -2,18 +2,23 @@ package middleware
 
 import (
 	"net/http"
+	"sync/atomic"
 
 	"github.com/cerfical/muzik/internal/log"
 )
 
 func LogRequest(l *log.Logger) func(http.Handler) http.Handler {
+	var requestCount atomic.Uint64
+
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			ll := l.WithFields(
+			requestID := requestCount.Add(1)
+
+			ll := l.WithFields("request_id", requestID)
+			ll.WithFields(
 				"method", r.Method,
 				"path", r.URL.Path,
-			)
-			ll.Info("Incoming request")
+			).Info("Incoming request")
 
 			rr := r.WithContext(ll.WithContext(r.Context()))
 			next.ServeHTTP(w, rr)
